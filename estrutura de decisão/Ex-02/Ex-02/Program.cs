72c3277b531ceb3c2f8e813a7b75d3a325971345
Ryan2104 <ryansilva2125@outlook.com>
2024-07-08 18:42:28 -0300

    ex-02

---
+++ estrutura de decisão/Ex-02/Ex-02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double cl;
+
+        Console.Write("Valor da conta de luz: ");
+        cl = double.Parse(Console.ReadLine());
+        if (cl > 50.00)
+            Console.Write("Você esta gastando muito");
+        else
+            Console.Write("Seu gasto foi normal ");
+    }
+}
--- estrutura de decisão/ex-03/ex-03/obj/Debug/net8.0/ex-03.AssemblyInfo.cs
+++ estrutura de decisão/ex-03/ex-03/obj/Debug/net8.0/ex-03.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ex-01")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ex-03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ex-01")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ex-01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4cd1685ed7ce34d844f75fc8275b2f6e3ea1a70")]
+[assembly: System.Reflection.AssemblyProductAttribute("ex-03")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ex-03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
