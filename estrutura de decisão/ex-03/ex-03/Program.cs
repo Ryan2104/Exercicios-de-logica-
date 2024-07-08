1c675c1efb2b787e743470d6e62eee0da3a036c0
Ryan2104 <ryansilva2125@outlook.com>
2024-07-08 19:05:02 -0300

    ex-03

--- estrutura de decisão/ex-03/ex-03/Program.cs
+++ estrutura de decisão/ex-03/ex-03/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double h, hom, mul;
+        Console.Write("qual a sua altura: ");
+        h = double.Parse(Console.ReadLine());
+        Console.Write("qual é o seu sexo: ");
+        string s = (Console.ReadLine());
+        string ho = "homem";
+        if (s == ho)
+        {
+            hom = (72.7 * h) - 58;
+            Console.Write("seu peso ideal é de: {0:F2} ", hom);
+        }
+        else if (s == "mulher")
+        {
+            mul = (62.1 * h) - 44.7;
+            Console.Write("O seu peso ideal é de: {0:F2}", mul);
+        }
+        else
+        {
+            Console.Write("resposta invalida");
+        }
+
+    }
+}
