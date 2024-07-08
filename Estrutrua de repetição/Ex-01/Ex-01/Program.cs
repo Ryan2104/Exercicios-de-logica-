a4cd1685ed7ce34d844f75fc8275b2f6e3ea1a70
Ryan2104 <ryansilva2125@outlook.com>
2024-07-08 18:29:42 -0300

    lista 1

---
+++ Estrutrua de repetição/Ex-01/Ex-01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double cl;
+        Console.Write("Valor da conta de luz: R$ ");
+        cl = double.Parse(Console.ReadLine());
+        if (cl > 50.00)
+            Console.Write("você esta gastando muito");
+
+    }
+}
